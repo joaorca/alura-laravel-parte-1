95d6213804c596c21c199fce6a6efb353ba06ff4
João R C Almeida <joaorca@unisuam.edu.br>
2020-04-20 23:29:56 -0300

    Introdução ao Laravel

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/series', function () {
+    $series = [
+        'Grey\'s Anatomy',
+        'Lost',
+        'Agents of SHILED'
+    ];
+
+    $html = "<ul>";
+    foreach ($series as $serie) {
+        $html .= "<li>$serie</li>";
+    }
+    $html .= "</ul>";
+
+    return $html;
+});
